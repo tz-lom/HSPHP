47996a5886db13bd581f1bba3c1771dc12a141a5
Nuzhdin Urii <nuzhdin.urii@gmail.com>
2010-11-05 19:36:05 +0300

    Pipeline extension for sockets

---
+++ library/Pipeline.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace HandlerSocket;
+
+class Pipeline
+{
+	/**
+	 * @var array
+	 */
+	protected $queue = array();
+	/**
+	 * @var ReadSocket | WriteSocket
+	 */
+	protected $socket = NULL;
+	
+	public function __construct($socket)
+	{
+		$this->socket = $socket;
+		$this->reset();
+	}
+	
+	public function reset()
+	{
+		$this->queue = array();
+		$this->accumulate = true;
+	}
+	
+	public function execute()
+	{
+		foreach($this->queue as $call)
+		{
+			call_user_func_array(array($this->socket,$call['method']),$call['args']);
+		}
+		$ret = array();
+		foreach($this->queue as $call)
+		{
+			$ret[] = call_user_func($call['callback'],$this->socket->readResponse());
+		}
+		return $ret;
+	}
+	
+	protected function null_callback($ret){return $ret;}
+	
+	public function registerCallback($callback=NULL)
+	{
+		if($callback==NULL) $callback=$this->null_callback;
+		$this->queue[count($this->queue)-1]['callback']=$callback;
+		return NULL;
+	}
+	
+	protected function addToQueue($item)
+	{
+		$this->queue[]=$item;
+	}
+	
+	public function select($index,$compare,$keys,$limit=1,$begin=0)
+	{
+		$this->addToQueue(array('method'=>'select','args'=>func_get_args()));
+	}
+	
+	public function update($index,$compare,$keys,$values,$limit=1,$begin=0)
+	{
+		$this->addToQueue(array('method'=>'update','args'=>func_get_args()));
+	}
+	
+	public function delete($index,$compare,$keys,$limit=1,$begin=0)
+	{
+		$this->addToQueue(array('method'=>'delete','args'=>func_get_args()));
+	}
+	
+	public function insert($index,$values)
+	{
+		$this->addToQueue(array('method'=>'insert','args'=>func_get_args()));
+	}
+	
+	public function openIndex($index,$db,$table,$key,$fields)
+	{
+		$this->addToQueue(array('method'=>'openIndex','args'=>func_get_args()));
+	}
+	
+	public function getIndexId($db,$table,$key,$fields)
+	{
+		return $this->socket->getIndexId($db,$table,$key,$fields);
+	}
+}
---
+++ tests/PipelineTest.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once('../library/IOException.php');
+require_once('../library/ErrorMessage.php');
+require_once('../library/ReadSocket.php');
+require_once('../library/ReadHandler.php');
+require_once('../library/WriteSocket.php');
+require_once('../library/WriteHandler.php');
+require_once('../library/Pipeline.php');
+
+class HSPipelineTest extends \HandlerSocket\WriteHandler
+{
+	function __construct($io)
+	{
+		$db = 'HSPHP_test';
+		if(file_exists('./my.cfg'))
+		{
+			$db = trim(file_get_contents('./my.cfg'));	
+		}
+		parent::__construct($io,$db,'write1',array('k'),'',array('k','v'));
+	}
+}
+
+class PipelineTest extends PHPUnit_Framework_TestCase
+{
+	protected $db = 'HSPHP_test';
+	
+	function __construct()
+	{
+		if(file_exists('./my.cfg'))
+		{
+			$this->db = trim(file_get_contents('./my.cfg'));	
+		}
+	}
+	
+	function testMultipleSelect()
+	{
+		$io = new \HandlerSocket\ReadSocket();
+		$io->connect();
+		
+		$pipe = new \HandlerSocket\Pipeline($io);
+		
+		$accessor = new \HandlerSocket\ReadHandler($pipe,$this->db,'read1',array('key'),'',array('float'));
+		
+		$accessor->select('=',42);
+		$accessor->select('=',12);
+		
+		$this->assertEquals(array(array(array('float'=>'3.14159')),array(array('float'=>'12345'))),$pipe->execute());
+	}
+	
+	function testBigChain()
+	{
+		$io = new \HandlerSocket\WriteSocket();
+		$io->connect();
+		
+		$pipe = new \HandlerSocket\Pipeline($io);
+		
+		$accessor = new \HandlerSocket\WriteHandler($pipe,$this->db,'write1',array('k'),'',array('k','v'));
+		
+		$accessor->select('=',12);
+		$accessor->insert(array('k'=>12,'v'=>'v12'));
+		$accessor->select('=',12);
+		$accessor->update('=',12,array('k'=>12,'v'=>'u12'));
+		$accessor->select('=',12);
+		$accessor->delete('=',12);
+		$accessor->select('=',12);
+		
+		$ret = $pipe->execute();
+		$this->assertEquals(array(),$ret[0]);
+		$this->assertTrue($ret[1]);
+		$this->assertEquals(array(array('k'=>'12','v'=>'v12')),$ret[2]);
+		$this->assertEquals(1,$ret[3]);
+		$this->assertEquals(array(array('k'=>'12','v'=>'u12')),$ret[4]);
+		$this->assertEquals(1,$ret[5]);
+		$this->assertEquals(array(),$ret[6]);
+	}
+}
