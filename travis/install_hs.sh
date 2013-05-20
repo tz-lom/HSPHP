639122a554f5acea86295274ce2dbbabb853139c
Nuzhdin Urii aka tz-lom <nuzhdin.urii@gmail.com>
2013-05-20 13:43:37 +0400

    Fix travis scripts

--- travis/install_hs.sh
+++ travis/install_hs.sh
@@ -1,7 +1,8 @@
-sudo apt-get update
-sudo apt-get install mysql-source-5.5
+sudo aptitude update
+sudo aptitude install mysql-server-5.5 mysql-client-5.5 libmysqlclient-dev mysql-source-5.5
 git clone https://github.com/DeNA/HandlerSocket-Plugin-for-MySQL.git hs
 cd hs
+git checkout 1.1.1
 ./autogen.sh
 cp /usr/src/mysql/mysql-source-5.5.tar.gz .
 tar -zxf mysql-source-5.5.tar.gz
